8c3a287c3982d7fa9c2be18c8c0ae0f644415dac
FelipeBrenner <felipeobrenner@hotmail.com>
2022-06-06 21:54:11 -0300

    Implementado sticker com click

--- TrabalhoPraticoGB/src/index.py
+++ TrabalhoPraticoGB/src/index.py
@@ -1,19 +1,26 @@
+from re import I
 import cv2
 import numpy as np 
 
-#read video
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(1) # read video
 
-#create a window
-cv2.namedWindow('Instagram')
+cv2.namedWindow('Instagram') # create a window
 
-#filter buttons
 isActiveBlur = False
 isActiveGray = False
 isActiveCanny = False
 isActiveEqualize = False
 isActiveDilate = False
 isActiveErode = False
+
+globalX = None
+globalY = None
+
+stickerIndex = 0
+stickers = [
+  None, # the background class has no sticker
+  cv2.imread("../stickers/sticker1.png", cv2.IMREAD_UNCHANGED),
+]
 
 def handleBlur(*args):
   global isActiveBlur
@@ -46,8 +53,68 @@ cv2.createButton('Equalize', handleEqualize, None, cv2.QT_PUSH_BUTTON, 1)
 cv2.createButton('Dilate', handleDilate, None, cv2.QT_PUSH_BUTTON, 1)
 cv2.createButton('Erode', handleErode, None, cv2.QT_PUSH_BUTTON, 1)
 
-while True: #continue to run until user breaks loop
+def stickerTransparent(background, sticker, x_offset=None, y_offset=None):
+  bg_h, bg_w, bg_channels = background.shape
+  fg_h, fg_w, fg_channels = sticker.shape
+
+  assert bg_channels == 3, f'background image should have exactly 3 channels (RGB). found:{bg_channels}'
+  assert fg_channels == 4, f'sticker image should have exactly 4 channels (RGBA). found:{fg_channels}'
+
+  # center by default
+  if x_offset is None: x_offset = (bg_w - fg_w) // 2
+  if y_offset is None: y_offset = (bg_h - fg_h) // 2
+
+  w = min(fg_w, bg_w, fg_w + x_offset, bg_w - x_offset)
+  h = min(fg_h, bg_h, fg_h + y_offset, bg_h - y_offset)
+
+  if w < 1 or h < 1: return
+
+  # clip sticker and background images to the overlapping regions
+  bg_x = max(0, x_offset)
+  bg_y = max(0, y_offset)
+  fg_x = max(0, x_offset * -1)
+  fg_y = max(0, y_offset * -1)
+  sticker = sticker[fg_y:fg_y + h, fg_x:fg_x + w]
+  background_subsection = background[bg_y:bg_y + h, bg_x:bg_x + w]
+
+  # separate alpha and color channels from the sticker image
+  sticker_colors = sticker[:, :, :3]
+  alpha_channel = sticker[:, :, 3] / 255  # 0-255 => 0.0-1.0
+
+  # construct an alpha_mask that matches the image shape
+  alpha_mask = np.dstack((alpha_channel, alpha_channel, alpha_channel))
+
+  # combine the background with the sticker image weighted by alpha
+  composite = background_subsection * (1 - alpha_mask) + sticker_colors * alpha_mask
+
+  # overwrite the section of the background image that has been updated
+  background[bg_y:bg_y + h, bg_x:bg_x + w] = composite
+
+  return background
+
+def mouseCallback(event, x, y, flags, param):
+  global globalX, globalY
+  if event == cv2.EVENT_LBUTTONDOWN:
+    globalX = x
+    globalY = y
+
+cv2.setMouseCallback('Instagram', mouseCallback)
+
+while True:
   ret, img = cap.read()
+
+  if(globalX is not None and globalY is not None):
+    sticker = stickers[1]
+
+    scale_percent = 30 # percent of original size
+    width = int(sticker.shape[1] * scale_percent / 100)
+    height = int(sticker.shape[0] * scale_percent / 100)
+    dim = (width, height)
+      
+    # resize image
+    sticker = cv2.resize(sticker, dim, interpolation = cv2.INTER_AREA)
+
+    img = stickerTransparent(img, sticker, int((globalX - sticker.shape[0]/2)), int((globalY - sticker.shape[1]/2)))
 
   if isActiveBlur:
     img = cv2.GaussianBlur(img, (13, 13), 5, 0)
@@ -66,15 +133,12 @@ while True: #continue to run until user breaks loop
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5));
     img = cv2.erode(img, kernel)
 
-  #display image
-  cv2.imshow('Instagram', img)
+  cv2.imshow('Instagram', img) # display image
 
-  #armazena o que foi digitado no teclado
-  k = cv2.waitKey(20) & 0xFF
+  k = cv2.waitKey(20) & 0xFF # armazena o que foi digitado no teclado
 
-  #if user pressed 'esc' break
-  if k == 27:
+  if k == 27: # if user pressed 'esc' break
     break
 
-cap.release() #turn off camera 
-cv2.destroyAllWindows() #close all windows
+cap.release() # turn off camera 
+cv2.destroyAllWindows() # close all windows
